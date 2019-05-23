75e7e4bc9285495ebd6239091da6e76e3325f6ae
Thales <thalesfraymond@gmail.com>
2019-05-23 17:21:34 -0300

    initialize population

---
+++ domain/algoritmo_genetico.py
@@ -0,0 +1,15 @@
+from domain.individuo import Individuo
+
+class AlgoritmoGenetico():
+    def __init__(self, tamanho_populacao):
+        self.tamanho_populacao = tamanho_populacao
+        self.populacao = []
+        self.geracao = 0
+        self.melhor_solucao = 0
+    
+    
+    def inicializar_populacao(self, espacos, valores, limites_espacos):
+        for i in range(self.tamanho_populacao):
+            self.populacao.append(Individuo(espacos,valores,limites_espacos))
+        
+        self.melhor_solucao = self.populacao[0]
